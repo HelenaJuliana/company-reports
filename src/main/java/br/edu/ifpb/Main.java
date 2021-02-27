43c95772d68f1370e9b395ba0ed3e6ca8cc1571d
Helena <helenajuliana14@gmail.com>
2021-02-27 16:21:52 -0300

    factory methody

--- src/main/java/br/edu/ifpb/Main.java
+++ src/main/java/br/edu/ifpb/Main.java
@@ -5,7 +5,9 @@ import br.edu.ifpb.report.service.ReportService;
 public class Main {
 
     public static void main(String[] args) {
-        ReportService reportService = new ReportService();
+       
+        DB db = new MysqlDatabase().getDatabase();
+//        ReportService reportService = new ReportService();
         reportService.generateReport("expense");
         reportService.generateReport("taxes");
     }
